8435ce62961740ee5cba6626bd382987f2ef99d1
Benshekniel <benshekniel@gmail.com>
2024-08-24 17:16:34 +0530

    some changes in usecontext

--- Frontend/src/context/UserContext.js
+++ Frontend/src/context/UserContext.js
@@ -3,14 +3,14 @@ import React, { createContext, useState } from 'react';
 export const UserContext = createContext();
 
 export const UserDataProvider = ({ children }) => {
-   const [user_data, setUserdata] = useState({
+   const [userdata, setUserdata] = useState({
       id: '',
       email: '',
-      role: 'User',
+      role: 'student',
    });
 
    return (
-      <UserContext.Provider value={{ user_data, setUserdata }}>
+      <UserContext.Provider value={{ userdata, setUserdata }}>
          {children}
       </UserContext.Provider>
    );
