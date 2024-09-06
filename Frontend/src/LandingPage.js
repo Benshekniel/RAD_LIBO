e0d85369c211199b4d7929fc1ca700c0d2a7a8b6
Benshekniel <benshekniel@gmail.com>
2024-09-06 18:39:48 +0530

    add navbar on landing page

--- Frontend/src/LandingPage.js
+++ Frontend/src/LandingPage.js
@@ -1,16 +1,28 @@
 import React from 'react';
 import './LandingPage.css';
-import { NavLink} from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+import Logo from './User/Assets/Logo.png';
 
 function LandingPage() {
   return (
     <div className="landing-container">
+      <div className="navBar">
+        <div className="nav-logo">
+          <img src={Logo} alt="Logo" />
+          <h1>Libo</h1>
+        </div>
+        <ul className="nav-links">
+          <li><NavLink to="/about" activeClassName="active-link">About</NavLink></li>
+        </ul>
+      </div>
+
       <div className="overlay">
         <div className="content">
           <h1>Welcome to Libo</h1>
-          <p>An Library management system for the Libraries</p>
-          
-          <NavLink to="/login"><button className="get-started-btn">Get started</button></NavLink>
+          <p>A Library Management System for Libraries</p>
+          <NavLink to="/login">
+            <button className="get-started-btn">Get started</button>
+          </NavLink>
         </div>
       </div>
     </div>
