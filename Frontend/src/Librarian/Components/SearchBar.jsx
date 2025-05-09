beff36c1928be7e875747a6757613566e40c1065
Benshekniel <benshekniel@gmail.com>
2025-05-09 18:56:06 +0530

    links changed

--- Frontend/src/Librarian/Components/SearchBar.jsx
+++ Frontend/src/Librarian/Components/SearchBar.jsx
@@ -34,10 +34,10 @@ const SearchBar = ({ onSearch }) => {
       </form>
       {studentData && (
         <div className="user-details">
-          <img src={`https://8840-2402-4000-2310-1e7d-d86d-cb02-e7bf-afe4.ngrok-free.app/image/${studentData.image}`} alt="User" className="user-avatar" />
+          <img src={`http://localhost:4000/image/${studentData.image}`} alt="User" className="user-avatar" />
           <div className="user-info">
             <p>{studentData.name}</p>
-            <p className="user-role">Student</p>
+            <p className="user-role">Librarian</p>
           </div>
         </div>
       )}
